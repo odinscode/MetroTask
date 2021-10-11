1c9c461d0bd30fe4b6411407cee80692783cf570
vodinokov <vodinokov@usetech.ru>
2021-10-11 13:52:43 +0300

    Init default console project

---
+++ MetroTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MetroTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
