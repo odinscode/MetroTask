1253687aac7722bb52bba2b8e08e055b4d94e97e
odinscode <32195465+odinscode@users.noreply.github.com>
2021-10-14 11:49:50 +0300

    Vending machine mvp logic (#2)
    * Add infinite loop for goods displaying
    
    * Add buy method
    
    * Update gitignore file for VS Code IDE
    
    * Change project type to ASP.NET Web API
    
    * Discard default web api controller, add basic logic for vending machine
    
    * Temp changes
    
    * Update temp logic for change transfer
    
    * Clean up code, update change output logic
    
    * Remove unused dependencies
    
    * Remove default web api occurances
    
    Co-authored-by: vodinokov <vodinokov@usetech.ru>

---
+++ Controllers/VendingMachineController.cs
@@ -0,0 +1,68 @@
+﻿using MetroTaskV2.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetroTaskV2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendingMachineController : ControllerBase
+    {
+        private readonly IVendingMachineService vmService;
+
+        public VendingMachineController(
+            IVendingMachineService vmService
+            )
+        {
+            this.vmService = vmService;
+        }
+
+        /// <summary>
+        /// Represenet the current stocks of vending machine.
+        /// </summary>
+        /// <returns>Collection of stocks with product type and available quantity.</returns>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var stocks = vmService.GetAllAvailableProductsInStock();
+
+            return stocks.Select(_ => (_).ToString());
+        }
+
+        /// <summary>
+        /// Customer's analog for depositing money into VM.
+        /// Purposly didn't make a validation for banknote denomination for easier testing purposes.
+        /// </summary>
+        /// <param name="banknote">Amount of money customer transfer on deposit of VM.</param>
+        /// <returns>Current deposited amount of money.</returns>
+        [HttpPut("[action]")]
+        public double DepositMoney(double banknote)
+        {
+            return vmService.DepositMoney(banknote);
+        }
+
+        /// <summary>
+        /// Buy selected product from VM.
+        /// </summary>
+        /// <param name="productName">Name of product to be purchased,</param>
+        /// <returns>Result of operation.</returns>
+        [HttpPost("[action]")]
+        public string Buy(string productName)
+        {
+            return vmService.Buy(productName);
+        }
+
+        /// <summary>
+        /// Give change from deposited money.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("[action]")]
+        public string GiveChange()
+        {
+            return vmService.GiveChange();
+        }
+    }
+}
---
+++ Services/VendingMachineService.cs
@@ -0,0 +1,157 @@
+﻿using MetroTaskV2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MetroTaskV2.Services
+{
+    /// <summary>
+    /// Implementation of IVendingMachineService.
+    /// </summary>
+    public class VendingMachineService : IVendingMachineService
+    {
+        internal static IDictionary<string, Stock> stocks = new Dictionary<string, Stock>();
+        internal static SortedDictionary<double, int> moneyForChange = new SortedDictionary<double, int>();
+        internal static double moneyForChangeTotal = 0;
+        internal static double depositMoney = 123.45;
+
+        static VendingMachineService()
+        {
+            var denominations = new double[] { 5000, 2000, 1000, 500, 200, 100, 50, 10, 5, 2, 1, 0.5, 0.1, 0.05, 0.01 };
+
+            foreach (var nominal in denominations)
+            {
+                moneyForChange.Add(nominal, 100);
+            }
+
+            CalculateTotalChangeAmountAvailable();
+
+            stocks = new Dictionary<string, Stock>()
+            {
+                { "beer", new Stock { Product = new Product { Name = "beer", Price = 40.5 }, Quantity = 2 } },
+                { "condoms", new Stock { Product = new Product { Name = "condoms", Price = 200.3 }, Quantity = 2 }},
+                { "cigarettes", new Stock { Product = new Product { Name = "cigarettes", Price = 150.43 }, Quantity = 2 } }
+            };
+        }
+
+        static void CalculateTotalChangeAmountAvailable()
+        {
+            moneyForChangeTotal = 0;
+
+            foreach (var item in moneyForChange)
+            {
+                moneyForChangeTotal += item.Key * item.Value;
+            }
+        }
+
+        public IEnumerable<Stock> GetAllAvailableProductsInStock()
+        {
+            return stocks.Values;
+        }
+
+        public double DepositMoney(double banknote)
+        {
+            // TO DO: add a validation of denomination of incoming banknote
+            // because customer can add only one banknote at time.
+            // Increase a banknote quantity in "moneyForChange" correspondingly of each denomination
+            // depending on incoming banknote denomination and execute "CalculateTotalChangeAmountAvailable()"
+
+            depositMoney += banknote;
+
+            return depositMoney;
+        }
+
+        public string Buy(string productName)
+        {
+            if (!stocks.ContainsKey(productName))
+            {
+                return $"Product '{productName}' is not exist.";
+            }
+
+            if (stocks[productName].Quantity <= 0)
+            {
+                return $"Product '{productName}' is out of stock.";
+            }
+
+            var remainingFunds = depositMoney - stocks[productName].Product.Price;
+
+            if (remainingFunds < 0)
+            {
+                return $"Insufficient deposited money! Please deposit at least {-remainingFunds} for purchasing '{productName}'";
+            }
+            
+            stocks[productName].Quantity -= 1;
+            depositMoney = remainingFunds;
+
+            return $"Successful purchased '{productName}'! Remaining funds {depositMoney}";
+        }
+
+        public string GiveChange()
+        {
+            if (depositMoney == 0)
+            {
+                return "Current deposit money is zero.";
+            }
+
+            if (moneyForChangeTotal < depositMoney)
+            {
+                return $"Sorry we can not provide change for your current deposit {depositMoney}. Please purchase something";
+            }
+
+            var changeToGive = depositMoney;
+            Dictionary<double, int> banknoteForChange = new Dictionary<double, int>();
+
+            foreach (var denomination in moneyForChange.Reverse())
+            {
+                if (changeToGive < 0.01)
+                {
+                    break;
+                }
+                // If denomination of banknote is greater than remainder of change 
+                // which should be paid - skip
+                // example: 5000 (denomination of banknote) > 43 (change to pay) -> skip
+                if (denomination.Key > changeToGive)
+                {
+                    continue;
+                }
+
+                // If banknote of current denomination is ended in VM.
+                if (denomination.Value < 1)
+                {
+                    continue;
+                }
+
+                var remainderOfBanknoteDenominationDivision = (int)(changeToGive / denomination.Key);
+                if (remainderOfBanknoteDenominationDivision > denomination.Value)
+                {
+                    banknoteForChange.Add(denomination.Key, denomination.Value);
+                    changeToGive -= denomination.Key * denomination.Value;
+                }
+                else
+                {
+                    banknoteForChange.Add(denomination.Key, remainderOfBanknoteDenominationDivision);
+                    changeToGive -= denomination.Key * remainderOfBanknoteDenominationDivision;
+                }
+            }
+
+            // we can give change
+            // update correspoding values in our banknote dictionary
+            // return info
+            if (changeToGive < 0.01)
+            {
+                var result = banknoteForChange
+                    .Select(_ => $"{_.Key} Ruble: {_.Value}")
+                    .Aggregate((a, b) => a + ", " + b);
+
+                return "Your change: " + result;
+            }
+
+            if (changeToGive > 0.01)
+            {
+                return $"Sorry we can not provide change for your current deposit {depositMoney}. Please purchase something";
+            }
+
+            return "Something went wrong!";
+        }
+    }
+}
